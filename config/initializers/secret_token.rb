e4778b0999c418ae746ed6b1a3803e2fb2d5c556
Jonathan Tushman <jtushman@pacwestcp.com>
2012-04-04 13:56:39 -0400

    initial dump

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tushman::Application.config.secret_token = '65676bf5428b49109d3ada2a9b701618438843610c4fed0d6a727b1c85f445e309bb23619020022d91f4901d5010eda296d1ae0a03b52f34e38086746dfa754a'
