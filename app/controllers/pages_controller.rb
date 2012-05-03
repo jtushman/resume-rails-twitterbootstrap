c598286e6837efa25c87599a6231dbc2f676963f
Jonathan Tushman <jtushman@pacwestcp.com>
2012-05-03 17:26:19 -0400

    adding the beginning of music

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
 
   def home; end
   def resume; end
+  def music
+    @albums = YAML::load(File.open("config/music.yml"))
+    puts @albums.inspect
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tushman::Application.routes.draw do
 
   get "resume" => "pages#resume", :as => 'resume'
+  get "music" => "pages#music", :as => 'music'
   get "home" => "pages#home", :as => 'home'
 
   root :to => 'pages#home'
